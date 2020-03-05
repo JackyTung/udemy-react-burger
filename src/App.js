ba39bba3f86186ffe5fb9ffb752fc1df089178a4
Jacky Tung <Jacky_Tung@htc.com>
2020-03-05 22:20:28 +0800

    init github page deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Router } from 'react-router';
-import { Redirect, Route, Switch } from 'react-router-dom';
+//import { Router } from 'react-router';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import { createBrowserHistory } from 'history';
 
@@ -9,10 +9,10 @@ import Console from './containers/Console';
 const history = createBrowserHistory();
 
 export default () => (
-  <Router history={history}>
+  <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
     <Switch>
       <Route path="/console" component={Console} />
       <Redirect to="/console" />
     </Switch>
-  </Router>
+  </BrowserRouter>
 );
