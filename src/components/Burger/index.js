3c9912ae18fdcca1d37abac72e026a944f3620af
Jacky Tung <Jacky_Tung@htc.com>
2020-04-05 20:52:26 +0800

    init build control

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import isEmpty from 'lodash/isEmpty';
 
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 
@@ -9,9 +12,30 @@ const useStyles = makeStyles(createStyles(styles));
 
 const Burger = props => {
   const classes = useStyles();
+  const ingredients = useSelector(state => state.ingredients);
+  const transformIngredients = Object.keys(ingredients)
+    .map(igKey => {
+      return [...Array(ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (isEmpty(transformIngredients)) {
+    return (
+      <div className={classes.Burger}>
+        <BurgerIngredient type="bread-top" />
+        <p>Please start add ingredients</p>
+        <BurgerIngredient type="bread-bottom" />
+      </div>
+    );
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
+      {transformIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
